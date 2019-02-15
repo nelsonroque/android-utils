a0937d2dc04aaa70496be6388865ebbb2c2bcc92
Nelson Roque <nelsonroquejr@gmail.com>
2019-02-15 14:24:51 -0500

    up

--- record_screen.bat
+++ record_screen.bat
@@ -1,4 +1,4 @@
 for /f %%# in ('wMIC Path Win32_LocalTime Get /Format:value') do @for /f %%@ in ("%%#") do @set %%@
 adb shell screenrecord /sdcard/screen_%month%_%day%_%year%-%hour%_%minute%_%second%.mp4
 cd captures
-adb pull /sdcard/screen_%month%_%day%_%year%-%hour%_%minute%_%second%.mp4 captures/videos/
+adb pull /sdcard/screen_%month%_%day%_%year%-%hour%_%minute%_%second%.mp4
