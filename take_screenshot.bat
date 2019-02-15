db86e51deb0ba8427149bbd10320e025110878cd
Nelson Roque <nelsonroquejr@gmail.com>
2019-02-15 17:50:34 -0500

    up

--- take_screenshot.bat
+++ take_screenshot.bat
@@ -1,4 +1,4 @@
 for /f %%# in ('wMIC Path Win32_LocalTime Get /Format:value') do @for /f %%@ in ("%%#") do @set %%@
-adb shell screencap -p /sdcard/screencap_%month%_%day%_%year%-%hour%_%minute%_%second%.png
-cd captures
-adb pull /sdcard/screencap_%month%_%day%_%year%-%hour%_%minute%_%second%.png
+adb shell screencap -p /sdcard/screenshot_%month%_%day%_%year%-%hour%_%minute%_%second%.png
+cd captures/screenshots
+adb pull /sdcard/screenshot_%month%_%day%_%year%-%hour%_%minute%_%second%.png
